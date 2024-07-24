7c32a93db63a586b8bc38ef9ca676850bf9324ac
Nicolas Soares <82896317+nsosoares@users.noreply.github.com>
2024-07-23 22:47:25 -0300

    Acrescentando númerico nas senhas

--- src/terraform-infra/modules/cognito/resources.tf
+++ src/terraform-infra/modules/cognito/resources.tf
@@ -2,4 +2,9 @@ resource "random_password" "master_password" {
   count = 2
   length  = 16
   special = true
+  numeric = true
+  min_lower = 1
+  min_numeric = 1
+  min_special = 1
+  min_upper = 1
 }
